e887a13da88e047ea91c0c2606c5cf533763975d
Steven Pickles <stevenpickles@gmail.com>
2020-03-14 14:31:00 -0400

    Refactor PowerShell script for easier readability

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,22 +1,12 @@
-﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://www.aplusfreeware.com/categories/LFWV/files/spcmn140.zip'
+﻿$packageName    = 'spacemonger.portable'
+$url            = 'http://www.aplusfreeware.com/categories/LFWV/files/spcmn140.zip'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checksum       = '662378EEA5A0E74DA3C33E54735C7C4EFD0FB44D9E739A246DA478F80D6296BF'
+$checksumType   = 'sha256'
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-
-  softwareName  = 'spacemonger.portable*'
-
-  checksum      = '662378EEA5A0E74DA3C33E54735C7C4EFD0FB44D9E739A246DA478F80D6296BF'
-  checksumType  = 'sha256'
-
-  silentArgs   = ''
-  validExitCodes= @(0)
-}
-
-Install-ChocolateyZipPackage $packageName $url $toolsDir
-
-
+Install-ChocolateyZipPackage `
+    -PackageName    $packageName `
+    -Url            $url `
+    -UnzipLocation  $toolsDir `
+    -Checksum       $checksum `
+    -ChecksumType   $checksumType
